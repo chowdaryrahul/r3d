67e4c435906baca1e099d973858c29eda92367db
smaurya30 <89805419+smaurya30@users.noreply.github.com>
2022-05-07 02:40:13 -0400

    Update Like.js

--- client/src/components/Like.js
+++ client/src/components/Like.js
@@ -7,62 +7,62 @@ import { AuthContext } from "../firebase/Auth";
 import { HeartIcon } from "@heroicons/react/outline";
 
 const Like = (props) => {
-  const { isValidUser , user } = useContext(AuthContext);
+  const { isValidUser, user } = useContext(AuthContext);
   let itemToLike = props.itemDataToLike;
   const [likeImage] = useMutation(queries.LIKE_ITEM);
   const [unlikeImage] = useMutation(queries.UNLIKE_ITEM);
 
   let likeBody = null;
-  if(isValidUser) {
-  if (props.likeFlag == "toLike") {
-    likeBody = (
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          likeImage({
-            variables: {
-              _id: itemToLike._id,
-              // user_id: mongoose.Types.ObjectId(user.uid),
-              user_id: user.uid,
-              user_name: itemToLike.user_name,
-              totalLikes: itemToLike.totalLikes,
-            },
-          });
-        }}
-      >
-        <button type="submit">
-		<HeartIcon class="fill-red-500 hover:fill-red-300"></HeartIcon>
-          Like
-		  </button>
-      </form>
-    );
+  if (isValidUser) {
+    if (props.likeFlag == "toLike") {
+      likeBody = (
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            likeImage({
+              variables: {
+                _id: itemToLike._id,
+                // user_id: mongoose.Types.ObjectId(user.uid),
+                user_id: user.uid,
+                user_name: itemToLike.user_name,
+                totalLikes: itemToLike.totalLikes,
+              },
+            });
+          }}
+        >
+          <button type="submit">
+            <HeartIcon class="fill-red-500 hover:fill-red-300"></HeartIcon>
+            {itemToLike.totalLikes}
+          </button>
+        </form>
+      );
+    } else {
+      likeBody = (
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            unlikeImage({
+              variables: {
+                _id: itemToLike._id,
+                // user_id: mongoose.Types.ObjectId(user.uid),
+                user_id: user.uid,
+                user_name: itemToLike.user_name,
+                totalLikes: itemToLike.totalLikes,
+              },
+            });
+          }}
+        >
+          <button type="submit">
+            <HeartIcon class="fill-red-500 hover:fill-red-300"></HeartIcon>
+            {itemToLike.totalLikes}
+          </button>
+        </form>
+      );
+    }
   } else {
-    likeBody = (
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          unlikeImage({
-            variables: {
-              _id: itemToLike._id,
-              // user_id: mongoose.Types.ObjectId(user.uid),
-              user_id: user.uid,
-              user_name: itemToLike.user_name,
-              totalLikes: itemToLike.totalLikes,
-            },
-          });
-        }}
-      >
-        <button type="submit">
-		<HeartIcon class="fill-red-500 hover:fill-red-300"></HeartIcon>
-          Unlike
-		  </button>
-      </form>
-    );
+    // Redirect to sign in page
+    console.log("Not logged in");
   }
-} else {
-  // Redirect to sign in page
-  console.log("Not logged in");
-}
 
   return <div>{likeBody}</div>;
 };
