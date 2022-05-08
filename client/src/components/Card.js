dea921ac24cb17f5db04f8f4457e368677f08946
smaurya30 <89805419+smaurya30@users.noreply.github.com>
2022-05-08 05:00:58 -0400

    UI Update

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -10,50 +10,54 @@ const Card = (props) => {
   let likedIdx = false;
   return (
     <div className="flex items-stretch ">
-      <ul className="grid sm:block md:grid md:block grid-cols-4 grid-rows-4 gap-4 items-center  flex items-stretch ">
+      <ul className="grid border-transparent sm:block md:grid md:block grid-cols-4 grid-rows-4 gap-4 items-center  flex items-stretch ">
         {cardsItemsData.map((items) => (
-          <div
-            key={items._id}
-            className="rounded-lg shadow-lg bg-white border-indigo-600 border-4 p-2 bg-transparent"
-          >
+          <div key={items._id} className="rounded-lg bg-white bg-transparent">
             <Link to={`/itemview/${items._id}`}>
               <li className=" h-96">
-                <p className="font-medium text-3xl text-indigo-500 hover:text-blue-400 text-center font-sans">
-                  {items.title}
-                </p>
-                <br />
-                <p
+                <span
                   className="
                   rounded-lg
                   shadow-lg
                   bg-white
                   max-w-sm
                   border-indigo-600
-                  border-1"
+                  border-1
+                  "
                 >
                   <img
                     src={items.multiple_images_of_obj[0]}
-                    className="w-30 h-30 bg-slate-100  h-64"
+                    className="h-96 bg-slate-100  "
                   />
-                </p>
+                </span>
                 <br />
-                <h3 className="font-medium text-1xl text-indigo-500 hover:text-blue-400 text-center h-fit">
-                  {items.description}
-                </h3>
               </li>
             </Link>
-            {items.likeDetails.map((likes, idx) => {
-              if (isValidUser && likes.user_id === user.uid) {
-                // PENDING: use session user_id to check condition
-                likedIdx = true;
-              }
-            })}
-            {likedIdx === false ? (
-              <Like likeFlag="toLike" itemDataToLike={items} />
-            ) : (
-              <Like likeFlag="toUnlike" itemDataToLike={items} />
-            )}
-            {(likedIdx = false)}
+            <div className="space-x-0 lg:flex lg:space-x-28 ">
+              <div>
+                {items.likeDetails.map((likes, idx) => {
+                  if (isValidUser && likes.user_id === user.uid) {
+                    // PENDING: use session user_id to check condition
+                    likedIdx = true;
+                  }
+                })}
+                {likedIdx === false ? (
+                  <Like likeFlag="toLike" itemDataToLike={items} />
+                ) : (
+                  <Like likeFlag="toUnlike" itemDataToLike={items} />
+                )}
+                {(likedIdx = false)}
+              </div>
+              <div>
+                <p className="mt-1 text-m text-gray-700 hover:text-blue-400 text-center font-sans">
+                  {items.title}
+                </p>
+
+                <p className="mt-1 text-sm text-gray-500 hover:text-blue-400 text-center h-fit">
+                  {items.price}$
+                </p>
+              </div>
+            </div>
           </div>
         ))}
       </ul>
