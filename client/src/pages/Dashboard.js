1bbe4529b31b6e8aac28da237fb7ed719b41b2b9
aishwaryashirbhate <92288147+aishwaryashirbhate@users.noreply.github.com>
2022-05-15 14:38:19 -0400

    pagination added for dashboard

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,29 +1,47 @@
 import { useDispatch, useSelector } from "react-redux";
+import { useState } from "react";
 import { useQuery } from "@apollo/client";
 import queries from "../queries.js";
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 import { AuthContext } from "../firebase/Auth";
 import Card from "../components/Card.js";
 import Page404 from "./Page404.js";
+import ReactPaginate from "react-paginate";
+import { getDataFromTree } from "@apollo/client/react/ssr";
+import "../App.css";
+// import { PromiseProvider } from "mongoose";
+import { get } from "lodash";
 
-const Dashboard = () => {
+const Dashboard = (props) => {
   const { isValidUser, user } = useContext(AuthContext);
   const dispatch = useDispatch();
+  const [itemData, setitemData] = useState([]);
+  const [pagenumber, setPageNumber] = useState(0);
 
-  let { loading, error, data } = useQuery(queries.FETCH_ITEMS, {
-    fetchPolicy: "cache-and-network",
-  });
+  const itemperpage = 20;
+  const pagecount = Math.ceil(itemData.length / itemperpage);
+
+  props.client
+    .query({
+      query: queries.FETCH_ITEMS,
+    })
+    .then((res) => {
+      setitemData(res.data.fetchItems);
+    });
 
   let cardData = null;
-
-  cardData = data && data.fetchItems && (
-    <Card itemsDataInCard={data.fetchItems} />
-  );
-
-  if (error) {
-    return <Page404 />;
+  if (itemData.length != 0) {
+    cardData = (
+      <Card
+        itemsDataInCard={itemData.slice(
+          pagenumber * itemperpage,
+          pagenumber * itemperpage + itemperpage
+        )}
+      />
+    );
+  } else if ((itemData.length = 0)) {
+    <Page404 />;
   }
-
   return (
     <div className="flex-auto flex-col md:flex-row h-screen w-screen  font-sans overflow-scroll">
       <div className="min-h-full">
@@ -35,6 +53,21 @@ const Dashboard = () => {
           </div>
         </header>
         <main>
+          <ReactPaginate
+            class="pagination justify-content-center"
+            previousLabel={"Previous"}
+            nextLabel={"Next"}
+            pageCount={pagecount}
+            onPageChange={({ selected }) => {
+              setPageNumber(selected);
+            }}
+            renderOnZeroPageCount={null}
+            containerClassName={"paginationBttns"}
+            previousLinkClassName={"previousBttn"}
+            nextLinkClassName={"nextBttn"}
+            disabledClassName={"paginationDisabled"}
+            activeClassName={"paginationActive"}
+          />
           <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
             {/* Replace with your content */}
             <div className="px-4 py-6 sm:px-0">
