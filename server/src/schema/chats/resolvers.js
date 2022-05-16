9d2d82c1ee13a9325a68f5b4f3cecacafa29594e
bholetanaya2715 <bholetanaya2715@gmail.com>
2022-05-16 17:38:12 -0400

    updated chat

--- server/src/schema/chats/resolvers.js
+++ server/src/schema/chats/resolvers.js
@@ -1,64 +1,59 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 const Schema = mongoose.Schema,
-  ObjectId = Schema.ObjectId;
-import mongoosastic from "mongoosastic";
-import { PubSub } from "graphql-subscriptions";
-import { ApolloError } from "apollo-server-core";
+	ObjectId = Schema.ObjectId;
 
-const pubsub = new PubSub()
+import { PubSub } from 'graphql-subscriptions';
+import { ApolloError } from 'apollo-server-core';
+
+const pubsub = new PubSub();
 
 const Chat = mongoose.model(
-    "Chat",
-  
-    new Schema({
-      user_name: {
-        type: String,
-        required: true,
-      },
-      message:{
-          type:String,
-          required:true
-      },
-      date:{
-          type:String,
-          required:false
-      }
-    }).plugin(mongoosastic)
-  );
+	'Chat',
 
-  const resolvers = {
-      Query:{
-          allChats:async()=>{
-            const chats = await Chat.find({});
-            if(chats){
-               
-               return [...chats]
-            }else{
-                throw new ApolloError("No Chat found")
-            }
-            
-          }
-      },
-      Mutation:{
-          addChat:async(_, args)=>{
-                 
-            const chat = new Chat({...args});
-            chat.save();
-            if(chat){
-                console.log(chat)
-                pubsub.publish('TRIGGER_ADD_CHAT',{addedChat:chat})
-                return chat
-            }
-            else{
-                throw new ApolloError("Failed to send message")
-            }
+	new Schema({
+		user_name: {
+			type: String,
+			required: true,
+		},
+		message: {
+			type: String,
+			required: true,
+		},
+		date: {
+			type: String,
+			required: false,
+		},
+	})
+);
 
-          }
-      },
-      Subscription:{
-          addedChat:{
-              subscribe:()=> pubsub.asyncIterator(['TRIGGER_ADD_CHAT'])
-          }
-      }
-  }
-  export default resolvers;
+const resolvers = {
+	Query: {
+		allChats: async () => {
+			const chats = await Chat.find({});
+			if (chats) {
+				return [...chats];
+			} else {
+				throw new ApolloError('No Chat found');
+			}
+		},
+	},
+	Mutation: {
+		addChat: async (_, args) => {
+			const chat = new Chat({ ...args });
+			chat.save();
+			if (chat) {
+				console.log(chat);
+				pubsub.publish('TRIGGER_ADD_CHAT', { addedChat: chat });
+				return chat;
+			} else {
+				throw new ApolloError('Failed to send message');
+			}
+		},
+	},
+	Subscription: {
+		addedChat: {
+			subscribe: () => pubsub.asyncIterator(['TRIGGER_ADD_CHAT']),
+		},
+	},
+};
+export default resolvers;
